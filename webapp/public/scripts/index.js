21302bd7e18d59d1f1203d629b2510674b76c402
mike-ebrahimi <104879016+mike-ebrahimi@users.noreply.github.com>
2023-02-13 12:24:07 -0700

    Frontend and backend with Firebase

---
+++ webapp/public/scripts/index.js
@@ -0,0 +1,43 @@
+const loginElement = document.querySelector('#login-form');
+const contentElement = document.querySelector("#content-sign-in");
+const userDetailsElement = document.querySelector('#user-details');
+const authBarElement = document.querySelector("#authentication-bar");
+
+// Elements for sensor readings
+const lightElement = document.getElementById("light");
+
+
+// MANAGE LOGIN/LOGOUT UI
+const setupUI = (user) => {
+  if (user) {
+    //toggle UI elements
+    loginElement.style.display = 'none';
+    contentElement.style.display = 'block';
+    authBarElement.style.display ='block';
+    userDetailsElement.style.display ='block';
+    userDetailsElement.innerHTML = user.email;
+
+    // get user UID to get data from database
+    var uid = user.uid;
+    console.log(uid);
+
+    // Database paths (with user UID)
+    var dbPathLight = 'UsersData/' + uid.toString() + '/LightIntensity';
+
+    // Database references
+    var dbRefLight = firebase.database().ref().child(dbPathLight);
+
+    // Update page with new readings
+    dbRefLight.on('value', snap => {
+      lightElement.innerText = snap.val().toFixed(2);
+    });
+
+  // if user is logged out
+  } else{
+    // toggle UI elements
+    loginElement.style.display = 'block';
+    authBarElement.style.display ='none';
+    userDetailsElement.style.display ='none';
+    contentElement.style.display = 'none';
+  }
+}
